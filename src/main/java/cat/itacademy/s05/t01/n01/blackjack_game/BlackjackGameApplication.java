f36618a8bfa5147eebf4fe58965ad8d88279e9fd
Marina Aguiar <marinaaguiar.dev@gmail.com>
2024-08-05 19:01:54 +0200

    create project files

---
+++ src/main/java/cat/itacademy/s05/t01/n01/blackjack_game/BlackjackGameApplication.java
@@ -0,0 +1,13 @@
+package cat.itacademy.s05.t01.n01.blackjack_game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackjackGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlackjackGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cat/itacademy/s05/t01/n01/blackjack_game/BlackjackGameApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itacademy.s05.t01.n01.blackjack_game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlackjackGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
