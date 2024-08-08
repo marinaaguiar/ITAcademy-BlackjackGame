1c78bdb9a6a64512407eec922953478fbd6c65d1
Marina Aguiar <marinaaguiar.dev@gmail.com>
2024-08-08 19:52:32 +0200

    add GameRepository and PlayerRepository

--- src/main/java/cat/itacademy/s05/t01/n01/blackjack_game/repository/GameRepository.java
+++ src/main/java/cat/itacademy/s05/t01/n01/blackjack_game/repository/GameRepository.java
@@ -1,6 +1,5 @@
 package cat.itacademy.s05.t01.n01.blackjack_game.repository;
 
-
 import cat.itacademy.s05.t01.n01.blackjack_game.model.Game;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 
--- src/main/java/cat/itacademy/s05/t01/n01/blackjack_game/repository/PlayerRepository.java
+++ src/main/java/cat/itacademy/s05/t01/n01/blackjack_game/repository/PlayerRepository.java
@@ -3,5 +3,5 @@ package cat.itacademy.s05.t01.n01.blackjack_game.repository;
 import cat.itacademy.s05.t01.n01.blackjack_game.model.Player;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 
-public interface PlayerRepository extends ReactiveCrudRepository<Player, Long> {
+public interface PlayerRepository extends ReactiveCrudRepository<Player, String> {
 }
