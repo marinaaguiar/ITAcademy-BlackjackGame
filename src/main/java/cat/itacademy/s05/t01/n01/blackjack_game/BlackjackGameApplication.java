d1a0e070506039ce775569e28030ea25b66fbbee
Marina Aguiar <marinaaguiar.dev@gmail.com>
2024-10-02 15:46:07 +0200

    update dependency to conform to Swagger and webflux

--- src/main/java/cat/itacademy/s05/t01/n01/blackjack_game/BlackjackGameApplication.java
+++ src/main/java/cat/itacademy/s05/t01/n01/blackjack_game/BlackjackGameApplication.java
@@ -2,8 +2,11 @@ package cat.itacademy.s05.t01.n01.blackjack_game;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"cat.itacademy.s05.t01.n01.blackjack_game", "package.where.swaggerconfig.is.located"})
+
 public class BlackjackGameApplication {
 
 	public static void main(String[] args) {
--- src/main/java/cat/itacademy/s05/t01/n01/blackjack_game/SwaggerConfig.java
+++ src/main/java/cat/itacademy/s05/t01/n01/blackjack_game/SwaggerConfig.java
@@ -1,11 +1,10 @@
-package cat.itacademy.s05.t01.n01.blackjack_game.utils;
+package cat.itacademy.s05.t01.n01.blackjack_game;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springdoc.core.models.GroupedOpenApi;
-
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
